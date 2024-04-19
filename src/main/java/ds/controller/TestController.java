706a14cfb0ac3956e3852ade54247e474b9c5dbd
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2024-04-19 14:20:11 +0300

    added TestController and swagger dependency

---
+++ src/main/java/ds/controller/TestController.java
@@ -0,0 +1,18 @@
+package ds.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Тестовый контроллер", description = "Проверка работоспособности")
+@Hidden
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+}
