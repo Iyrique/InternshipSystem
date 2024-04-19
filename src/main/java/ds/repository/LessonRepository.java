e87c1085892bb751488f048f5c1174c4d0ecccb5
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2024-04-19 13:36:06 +0300

    added repository

---
+++ src/main/java/ds/repository/LessonRepository.java
@@ -0,0 +1,10 @@
+package ds.repository;
+
+import ds.domain.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+}
