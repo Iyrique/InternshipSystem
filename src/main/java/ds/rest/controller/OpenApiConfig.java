ecfae735fb3c9d33537aceed242bba365a4be5dc
Nikita Voronezhskiy <voronezhskij_n_m@sc.vsu.ru>
2024-05-10 09:54:58 +0300

    updated OpenApiConfig

--- src/main/java/ds/rest/controller/OpenApiConfig.java
+++ src/main/java/ds/rest/controller/OpenApiConfig.java
@@ -1,19 +1,20 @@
 package ds.rest.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Internship system DIGITAL SPIRIT",
+                description = "Internship system", version = "1.0.0",
+                contact = @Contact(
+                        name = "gitlab: voronezhskiy.nikita"
+                )
+        )
+)
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().info(new io.swagger.v3.oas.models.info.Info()
-                .title("Internship system DIGITAL SPIRIT")
-                .description("Internship system")
-                .version("1.0.0")
-                .contact(new io.swagger.v3.oas.models.info.Contact()
-                        .name("gitlab: voronezhskiy.nikita"))
-
-        );
-    }
 }
